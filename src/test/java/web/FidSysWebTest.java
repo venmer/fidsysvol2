debe22f75d1a4c29779a46398fca16517d839694
Maksim Remnev <remnev.maksim21@gmail.com>
2015-05-19 23:05:48 +0300

    web test: welcome page

--- src/test/java/web/FidSysWebTest.java
+++ src/test/java/web/FidSysWebTest.java
@@ -1,5 +1,6 @@
 package web;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
@@ -7,6 +8,10 @@ import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import ru.yandex.qatools.allure.annotations.Features;
 import ru.yandex.qatools.allure.annotations.Step;
 import ru.yandex.qatools.allure.annotations.Stories;
+
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 /**
  * autor:maksim
@@ -16,6 +21,7 @@ import ru.yandex.qatools.allure.annotations.Stories;
 @Features("Fidsys main page")
 public class FidSysWebTest {
    private WebDriver driver;
+    private final String WELCOME_PAGE="Welcome to FidSys";
     @Before
     public void createDriver(){
         driver=new PhantomJSDriver();
@@ -25,9 +31,17 @@ public class FidSysWebTest {
     @Test
     public void openMainPage(){
         openPage("http://localhost:8081");
+        String title=driver.getTitle();
+        assertThat(title,is(notNullValue()));
+        assertThat(title,is(WELCOME_PAGE));
     }
     @Step("Open {0}")
     public void openPage(String url){
         driver.get(url);
+    }
+    @After
+    public void closeDriver(){
+        driver.close();
+        driver.quit();
     }
 }
